932cd2cd4e65cbd9c7c6e77b06e1ebd9ede404bd
Ali Mehri <alimehridev@gmail.com>
2023-11-13 04:31:44 +0330

    requirements.txt added .

--- script.py
+++ script.py
@@ -8,20 +8,20 @@ from requests_html import HTMLSession
 import argparse
 
 
-parser = argparse.ArgumentParser(description="webCrawl is a very simple url crawler .", epilog="example: python script.py -u https://google.com --cookies xwidth=1920;xheight=1032;sessionid=ed83623d96a51fb1bc --depth 3 --threads 8 -o result.txt")
-parser.add_argument("-u", "--url", type=str, required=True, help="URL to crawl . [Required]")
-parser.add_argument("-c", "--cookies", type=str, default="", help="Set cookies if authentication is required, sperate them with ';' . example: xwidth=1920;xheight=1032;sessionid=ed83623d96a51fb1bc")
-parser.add_argument("-d", "--depth", type=str, default=2, help="Assign depth of crawl, default=2")
+parser = argparse.ArgumentParser(description="webCrawl is a very simple url crawler .", epilog="example: python script.py -u https://google.com --cookies xwidth=1920;xheight=1032;sessionid=ed83623d96a51fb1bc --depth 3 --threads 8 -o result.txt") 
+parser.add_argument("-u", "--url", type=str, required=True, help="URL to crawl . [Required]") # Works Prefectly
+parser.add_argument("-c", "--cookies", type=str, default="", help="Set cookies if authentication is required, s perate them with ';' . example: xwidth=1920;xheight=1032;sessionid=ed83623d96a51fb1bc")
+parser.add_argument("-d", "--depth", type=str, default=2, help="Assign depth of crawl, default=2") # Just full depth works
 parser.add_argument("-v", "--verbose", action='store_true', help="Set verboseness to show more logs")
 parser.add_argument("-ex", "--excludes", type=str, help='Set some filters that webCrawler should not crawl them, example "jpg,png,gif,css"')
 parser.add_argument("-in", "--includes", type=str, help='Set some filters that webCrawler should only crawl them, example "php,aspx,py"')
-parser.add_argument("-t", "--threads", type=int, default=3, help='Threads number, default=3 and max=16')
+parser.add_argument("-t", "--threads", type=int, default=3, help='Threads number, default=3 and max=16') # Works but can be better
 parser.add_argument("-a", "--assets", action="store_true", help='If crawling assets like js files is necessary .')
-parser.add_argument("-o", "--output_file", default="[url].txt", help='output_file to save the output . [default=[url].txt]')
+parser.add_argument("-o", "--output_file", default="[url].txt", help='output_file to save the output . [default=[url].txt]') # Can be removed .
 parser.add_argument("-f", "--format", default="plain_text", help='Format of output, default=plain_text [json is other option]')
 parser.add_argument("-m", "--morelogs", action="store_true", help='Save more logs like Status Code of each links .')
-parser.add_argument("-co", "--continue", action="store_true", help='Continue last crawl .')
-parser.add_argument("--onlydomain", action="store_true", help='Just save links in this domain and ignore others .')
+parser.add_argument("-co", "--continue", action="store_true", help='Continue last crawl .') # Works but check it 
+parser.add_argument("--onlydomain", action="store_true", help='Just save links in this domain and ignore others .') # Works but chekc it 
 
 args = vars(parser.parse_args())
 # {'url': 'https://google.com', 'cookies': '', 'depth': 2, 'verbose': True, 'exclude': None, 'threads': 3, 'assets': True, 'output_file': '[url].txt', 'format': 'plain_text', 'morelogs': True}
